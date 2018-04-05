6210dd58acc08d2e1ea08ceaceda381e90fbc0c3
p4kner <p4kner@gmail.com>
2018-04-05 15:03:03 +0200

    Installed Packages
    Installed packages listed in envpackages.txt via $ pip install -r filename.

---
+++ py3_packages_env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/pf_admn/work/dev/venv-deployment-test/py3_packages_env/bin/python3.5
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
